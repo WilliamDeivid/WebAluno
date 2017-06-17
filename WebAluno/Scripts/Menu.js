3401428858d4923f431e488d9f0272d5fd973a06
WilliamDeivid <williamdeividasilva@gmail.com>
2017-06-17 01:09:39 -0300

    Mudanças2

---
+++ WebAluno/Scripts/Menu.js
@@ -0,0 +1,20 @@
+﻿  function openNav(){
+      document.getElementById("SlideMenuNav").style.display="block"
+  }
+function closeNav(){
+    document.getElementById("SlideMenuNav").style.display="none"
+}
+function showresponddiv(e){
+    var n=e.replace("submenu-","opensubmenu-"),l=document.getElementById(n);current&&current!=l&&(current.style.display="none"),"none"==l.style.display?(l.style.display="block",current=l):l.style.display="none"
+}
+var current=null;
+
+
+function fixOpacity(){
+    document.getElementById("menuOpacity").style.opacity = "0.6";
+    document.getElementById("menuOpacity").style.transition = "0.4s";
+}
+function fixOpacity2(){
+    document.getElementById("menuOpacity").style.opacity = "1";
+    document.getElementById("menuOpacity").style.transition = "0.4s";
+}
