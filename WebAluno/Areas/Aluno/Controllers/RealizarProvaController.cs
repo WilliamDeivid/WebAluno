8e0d03943f7eaf0bf0073b5dc534f4924ec559a3
WilliamDeivid <williamdeividasilva@gmail.com>
2017-06-18 12:00:28 -0300

    Ajustes tela prova

---
+++ WebAluno/Areas/Aluno/Controllers/RealizarProvaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAluno.Areas.Aluno.Controllers
+{
+    public class RealizarProvaController : Controller
+    {
+        // GET: Aluno/RealizarProva
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
